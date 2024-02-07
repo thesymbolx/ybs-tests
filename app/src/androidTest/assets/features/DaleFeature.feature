89e6a84f013ccf00cb4b52f5c69045b2d7b7399d
Dale Evans <devans@ybs.co.uk>
2024-02-07 15:08:59 +0000

    Fix failing tests

--- app/src/androidTest/assets/features/DaleFeature.feature
+++ app/src/androidTest/assets/features/DaleFeature.feature
@@ -1,4 +1,7 @@
 # The first example has two steps
+
+Feature: Dale Evans
+
 Scenario: Maker starts a game
 When the Maker starts a game
 Then the Maker waits for a Breaker to join
--- app/src/androidTest/java/com/example/ybstests/TestStep.kt
+++ app/src/androidTest/java/com/example/ybstests/TestStep.kt
@@ -1,5 +1,6 @@
 package com.example.ybstests
 
+import android.util.Log
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
@@ -7,11 +8,11 @@ import io.cucumber.java.en.When
 class TestStep {
     @When("the Maker starts a game")
     fun a() {
-
+        //Log.d("test", "a() called")
     }
 
     @Then("the Maker waits for a Breaker to join")
     fun b() {
-
+        println("function b")
     }
 }
