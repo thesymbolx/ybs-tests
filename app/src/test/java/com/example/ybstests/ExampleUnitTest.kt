c4e29fdc7cd3dc52cb57fae51c8b4f52de13e870
Dale Evans <devans@ybs.co.uk>
2024-03-05 14:39:48 +0000

    Objects built

--- app/src/test/java/com/example/ybstests/ExampleUnitTest.kt
+++ app/src/test/java/com/example/ybstests/ExampleUnitTest.kt
@@ -1,5 +1,13 @@
 package com.example.ybstests
 
+import com.example.ybstests.chat.data.MessageDataSourceLocal
+import com.example.ybstests.chat.data.MessageDataSourceRemote
+import com.example.ybstests.chat.data.MessageRepo
+import com.example.ybstests.chat.domain.GetChatMessagesUseCase
+import com.example.ybstests.chat.domain.SendTextMessageUseCase
+import com.example.ybstests.chat.ui.ChatViewModel
+import com.example.ybstests.network.CacheApi
+import com.example.ybstests.network.MessageApi
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.junit.Test
@@ -7,6 +15,36 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class MyTests : StringSpec({
+
+    val fakeCache = object : CacheApi {
+        override fun saveMessage() {
+
+        }
+    }
+
+    val fakeApi = object : MessageApi {
+        override fun sendMessage() {
+
+        }
+
+        override fun getMessage(): List<String> {
+            return listOf(
+                "fake 1", "fake 2", "fake 2"
+            )
+        }
+    }
+
+    val messageRepo = MessageRepo(
+        messageDataSourceRemote = MessageDataSourceRemote(fakeApi),
+        messageDataSourceLocal = MessageDataSourceLocal(fakeCache)
+    )
+
+    val chatVm = ChatViewModel(
+        getChatMessagesUseCase = GetChatMessagesUseCase(messageRepo),
+        sendTextMessageUseCase = SendTextMessageUseCase(messageRepo),
+    )
+
+
     "strings.length should return size of string" {
         "hello".length shouldBe 5
     }
